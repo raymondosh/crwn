21f84de712b16a19b34f55a3d4ab47a78526fc6e
peoray <peoray@yahoo.com>
2019-11-17 09:07:03 +0100

    finish lesson-13

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import CheckoutItem from "../checkout-item/checkout-item.component";
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 import { createStructuredSelector } from "reselect";
 import {
   selectCartItems,
@@ -31,8 +32,15 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>${total}</span>
+      <span>TOTAL: ${total}</span>
+
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
